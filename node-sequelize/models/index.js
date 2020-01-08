9cf0cb772e29b78e7a36b8f0ef1cc8bc58b44db0
Rabidranath Ferreira <rabindranathucv@gmail.com>
2020-01-07 21:37:18 -0300

    change config

--- node-sequelize/models/index.js
+++ node-sequelize/models/index.js
@@ -13,7 +13,6 @@ loadConfig();
 
 let sequelize;
 if (config.use_env_variable) {
-    console.log(config);
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
