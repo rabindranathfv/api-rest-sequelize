7a257e135b7ab1a7922d4f8768a566714c5f9d59
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-08-18 03:02:02 -0400

    minor fix into config.js

--- config/config.js
+++ config/config.js
@@ -6,9 +6,7 @@ const dotenv = require('dotenv');
 
 const loadConfig = () => {
     if (process.env.NODE_ENV === 'production') {
-        console.log('running production', path.join(__dirname, './.env.prod'));
         dotenv.config({ path: path.join(__dirname, './.env.prod') });
-        console.log('running DB PROD:::', process.env.DATABASE_URL);
         process.env.DATABASE_URL = process.env.DATABASE_URL ? process.env.DATABASE_URL : `${process.env.DB_DIALECT}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
     } else if (process.env.NODE_ENV === 'staging') {
         dotenv.config({ path: path.join(__dirname, './.env.staging') });
